ec9f32675990e55e10892ea46b89ccee1d8de84a
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-09-08 19:20:36 +0530

    Correct regex for matching email in user model

--- app/models/user.js
+++ app/models/user.js
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        match: /.*@.*/
+        match: /.+@.+/
     },
     country_code: {
         type: Number,
