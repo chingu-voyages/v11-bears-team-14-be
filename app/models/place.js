3fc98c915b02cd0cfc5ce9e1cec40f8a842fb4be
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-09-08 19:22:59 +0530

    Modify places model to use reservation arr and is_available func

--- app/models/place.js
+++ app/models/place.js
@@ -32,7 +32,13 @@ const placeSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    available_dates: [ dateSchema ],
+    reservations: [
+       {
+         user_id: Number,
+         start_date: Date,
+         end_date: Date,
+       }
+    ],
     latitude: {
         type: String
     },
@@ -41,26 +47,45 @@ const placeSchema = mongoose.Schema({
     }
 });
 
-placeSchema.methods.isPlaceAvailable = (Place, startDate, endDate) => {
-    let availableDates = Place.available_dates;
-    return isValid(startDate, endDate, availableDates);
+// placeSchema.methods.isPlaceAvailable = (Place, startDate, endDate) => {
+//     let availableDates = Place.available_dates;
+//     return isValid(startDate, endDate, availableDates);
+// }
+
+placeSchema.method.is_available = function(startDate, endDate) {
+  const res = this.reservations.find((booking) => {
+    if (booking.start_date < startDate) {
+      if (bookings.end_date < startDate) {
+        return false;
+      } else {
+        return true;
+      }
+    } else if (booking.start_date <= endDate) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  if (!!res) {
+    return false;
+  }
 }
 
-function isValid(start, end, availableDates) {
-    let isStartDateAvailable = false;
-    let isEndDateAvailable = false;
-    for (let i = 0 ; i < availableDates.length ; i++) {
-        let date = availableDates[i].date;
-        let isDateAvailable = availableDates[i].is_available;
-        if (start == date && isDateAvailable) {
-            isStartDateAvailable = true;
-        } 
-        if (end == date && isDateAvailable) {
-            isEndDateAvailable = true;
-        }
-    }
-    return (isStartDateAvailable && isEndDateAvailable);
-}
+// function isValid(start, end, availableDates) {
+//     let isStartDateAvailable = false;
+//     let isEndDateAvailable = false;
+//     for (let i = 0 ; i < availableDates.length ; i++) {
+//         let date = availableDates[i].date;
+//         let isDateAvailable = availableDates[i].is_available;
+//         if (start == date && isDateAvailable) {
+//             isStartDateAvailable = true;
+//         }
+//         if (end == date && isDateAvailable) {
+//             isEndDateAvailable = true;
+//         }
+//     }
+//     return (isStartDateAvailable && isEndDateAvailable);
+// }
 
 // Creating a model for Place
 const Place = mongoose.model('Place', placeSchema);
