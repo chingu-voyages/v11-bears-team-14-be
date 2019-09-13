ddee32bd1a665768fc732028c2340fe4f37e647a
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-09-13 07:49:06 +0530

    Add GraphQL mutate schema for user

--- app/schema/RootMutation.js
+++ app/schema/RootMutation.js
@@ -4,7 +4,9 @@ const {
 } = require("graphql");
 
 const PersonType = require('../types/PersonType');
+const UserType = require('../types/UserType');
 const PersonResolver = require('../resolvers/person');
+const UserResolver = require('../resolvers/user');
 
 const mutation = {
     person: {
@@ -14,6 +16,17 @@ const mutation = {
             lastname: { type: GraphQLNonNull(GraphQLString) }
         },
         resolve: PersonResolver.Mutation.create
-    }
-}
+    },
+    user: {
+      type: UserType,
+      args: {
+        firstname: { type: new GraphQLNonNull(GraphQLString) },
+        lastname: { type: new GraphQLNonNull(GraphQLString) },
+        email: { type: new GraphQLNonNull(GraphQLString) },
+        password: { type: new GraphQLNonNull(GraphQLString) },
+        country: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve: UserResolver.Mutation.createUser,
+    },
+};
 module.exports = mutation;
