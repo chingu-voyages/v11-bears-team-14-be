9d3245923365991bcb570fbee4f316d1cbe51cd6
Shibani Shankar Dash <shibanidash71@gmail.com>
2019-09-15 18:14:28 +0530

    Verify jwt and attach user id to requests hence gql context

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ExpressGraphQL = require("express-graphql");
 const expressRateLimit = require('express-rate-limit');
+const { verify } = require('jsonwebtoken');
 require('dotenv').config();
 
 const { dbconnection } = require('./app/connection');
@@ -36,11 +37,40 @@ app.use(rateLimiter);
 // mongoose connection
 dbconnection();
 
+app.use((req, res, next) => {
+    const authHeader = req.get('Authorization');
+    if (!authHeader) {
+        req.isAuth = false;
+        return next();
+    }
+    const token = authHeader;
+    if (!token) {
+        req.isAuth = false;
+        return next();
+    }
+    try {
+        var isVerified = verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+        req.isAuth = false;
+        return next();
+    }
+    if (!isVerified) {
+        req.isAuth = false;
+        return next();
+    }
+    console.log('verified token in header',isVerified.id);
+    req.isAuth = true;
+    req.uid = isVerified.id;
+    return next();
+})
+
 // GraphQL
 app.use('/graphql', ExpressGraphQL({
     schema: schema,
     graphiql: true
 }));
+
+
 
 app.listen(port, () => {
     console.log('APP is listening on port:', port);
