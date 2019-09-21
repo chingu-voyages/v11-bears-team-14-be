a48c2aaccc1f66bf33ea2d49b55ef668984b83b8
karthickram286 <karthick.r@zohocorp.com>
2019-09-21 08:36:56 +0530

    test config added

--- app/connection/index.js
+++ app/connection/index.js
@@ -6,7 +6,12 @@ const config = require('config');
  * Connect to MongoDB.
  */
 exports.dbconnection = () => {
-    const mongoDB = config.get('db');
+    let mongoDB;
+    if (config.get('node_process') == 'test') {
+        mongoDB = "mongodb://karthickr:" + config.get('testMongoPwd') + "@ds247171.mlab.com:47171/v11-bears-team-14-be";
+    } else {
+        mongoDB = "mongodb://localhost:27017/playground";
+    }
     mongoose.set('useCreateIndex', true);
     mongoose.set('useNewUrlParser', true);
     mongoose.set('useFindAndModify', false);
